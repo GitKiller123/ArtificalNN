def2a7d9776798e8fcfd7af3f27a64d21ec31896
GitKiller123 <jonasalexandersson@hotmail.com>
2018-10-16 21:59:33 +0200

    HW3 task 1

--- Homework3/Task1/Task1_1.m
+++ Homework3/Task1/Task1_1.m
@@ -9,17 +9,15 @@ training_set_out = tTrain';
 validation_set_in = xValid';
 validation_set_out = tValid';
 
-x_avg = mean(xTrain,2);
-
 T = 30;
-mu = 0.03;
+mu = 0.3;
 epoch = size(training_set_in,1);
 size_batch = 10;
 M1 = 784; %Amount of neurons in 1st hidden layer
 M2 = 10; %Amount of neurons in 2nd hidden layer
 
-g = @(b)((1+exp(b)).^(-1));  %Activation function
-g_prim = @(b)(sech(b).^2); %Derivative of the activation function
+g = @(b)((1+exp(-b)).^(-1));  %Activation function
+g_prim = @(b)(g(b)-g(b).^2); %Derivative of the activation function
 % b = @(w,V,theta,i,l)(w{l}(:,i)'*V{l-1} - theta{l}(i));
 delta_func = @(w,V,theta,delta,i,j,l)(delta{l}(i)*w{l}(j,i)*g_prim(b(w,V,theta,j,l-1)));
 
@@ -35,6 +33,7 @@ for l = 2:length(N_neurons)
     end
     theta{l} = zeros(N_neurons{l},1); %Creating starting thresholds for each layer
 end
+x_avg = mean(training_set_in,1)';
 for epochs = 1:T
     sequence = randperm(epoch);
     counter = 1;
@@ -43,16 +42,18 @@ for epochs = 1:T
             delta_w{l} = zeros(N_neurons{l-1},N_neurons{l});
             delta_theta{l} = zeros(N_neurons{l},1);
         end
+        dH = 0;
         for test = 1:size_batch
             pattern = sequence(counter);
-%             numbers(test) = pattern;
-            V{1} = (training_set_in(pattern,:))';
+            numbers(test) = pattern;
+            V{1} = (training_set_in(pattern,:))'-x_avg;
             for l = 2:layers
                 b_value = w{l}'*V{l-1} - theta{l};
                 V{l} = g(b_value);
             end
+            O(test,:) = V{end};
             b_value = w{end}'*V{end-1} - theta{end};
-            delta{layers} = g_prim(b_value).*(training_set_out(pattern,:)'-V{end})./size_batch;
+            delta{layers} = g_prim(b_value).*(training_set_out(pattern,:)'-V{end});
             for l = layers:-1:3
                 b_value = w{l-1}'*V{l-2} - theta{l-1};
                 delta{l-1} = w{l}*delta{l}.*g_prim(b_value);
@@ -60,13 +61,27 @@ for epochs = 1:T
             for l = 2:layers
                 delta_w{l} = delta_w{l} + mu*V{l-1}*delta{l}';
                 delta_theta{l} = delta_theta{l} - mu*delta{l};
+                if isnan(sum(sum(delta_w{l})))
+                    k = 1
+                end
+                if isnan(sum(sum(delta_theta{l})))
+                    k = 1
+                end
             end
+            
             counter = counter + 1;
         end
         for l = 2:layers
             w{l} = w{l} + delta_w{l};
             theta{l} = theta{l} + delta_theta{l};
         end
+        for test = 1:size_batch
+            dH = dH + 0.5*(training_set_out(numbers(test),:)-O(test))*(training_set_out(numbers(test),:)-O(test))';
+        end
+%         if dH > 5
+%             dH = 0;
+%         end
+        H(trial+(epochs-1)*epoch/size_batch) = dH;
     end
     
     
@@ -82,16 +97,16 @@ for epochs = 1:T
             O = 1;
         end
     end
-    C = mean((1/(2*length(validation_set_out)))*sum(abs(round(O)-validation_set_out)))
+    C = mean((1/(2*length(validation_set_out)))*sum(abs(round(O)-validation_set_out)));
     if C<C_best
         C_best = C
         
     end
-    if C<0.12
-        w_best = w;
-        theta_best = theta;
-        break
-    end
+%     if C<0.12
+%         w_best = w;
+%         theta_best = theta;
+%         break
+%     end
 %     if sum(ismember(C_vector, 0.1516)) == 7
 %         break
 %     end
